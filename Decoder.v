ffb3d8e61070534f0a30fea2b7bc737a57eb5256
Sabadabadu <devidaleduard@gmail.com>
2021-02-13 10:22:07 +0100

    Add files via upload

---
+++ Decoder.v
@@ -0,0 +1,150 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/06/2021 07:43:14 PM
+// Design Name: 
+// Module Name: Decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Decoder(
+	input		[31:0]	inst,
+	            
+	output		[6:0]	funct7,
+	output		[2:0]	funct3,
+	output	reg	[31:0]	imm,
+	output		[4:0]	rs2,
+	output		[4:0]	rs1,
+	output		[4:0]	rd,
+	output		[6:0]	opcode
+    );
+
+	/* VARIABLES */
+
+	reg		[2:0]	inst_type;
+
+
+	/* DEFINITIONS */
+	
+	// Instruction types
+	parameter	R_TYPE	=	3'h0;
+	parameter	I_TYPE	=	3'h1;
+	parameter	S_TYPE	=	3'h2;
+	parameter	B_TYPE	=	3'h3;
+	parameter	U_TYPE	=	3'h4;
+	parameter	J_TYPE	=	3'h5;
+
+	// opcodes (the 2 LSB don't matter, they are always 11)
+	parameter	LOAD	=	5'b00000;
+	parameter	STORE	=	5'b01000;
+	parameter	BRANCH	=	5'b11000;
+                              
+	parameter	JALR	=	5'b11001;
+                              
+	parameter	JAL		=	5'b11011;
+                              
+	parameter	OP_IMM	=	5'b00100;
+	parameter	OP		=	5'b01100;
+//  parameter	SYSTEM	=	5'b11100;
+                              
+	parameter	AUIPC	=	5'b00101;
+	parameter	LUI		=	5'b01101;
+
+
+	/* IMPLEMENTATION */
+
+	assign funct7 = inst[31:25];
+	assign funct3 = inst[14:12];
+
+	// Decide which instruction type is inst
+	always @(*) begin
+		case(inst[6:2])
+			LOAD: begin
+				inst_type = I_TYPE;
+			end
+			STORE: begin
+				inst_type = S_TYPE;
+			end
+			BRANCH: begin
+				inst_type = B_TYPE;
+			end
+
+			JALR: begin
+				inst_type = I_TYPE;
+			end
+
+			JAL: begin
+				inst_type = J_TYPE;
+			end
+
+			OP_IMM: begin
+				inst_type = I_TYPE;
+			end
+			OP: begin
+				inst_type = R_TYPE;
+			end
+		/*  SYSTEM: begin
+				inst_type = ?_TYPE;
+			end */
+
+			AUIPC: begin
+				inst_type = U_TYPE;
+			end
+			LUI: begin
+				inst_type = U_TYPE;
+			end
+
+			default:
+				inst_type = 3'bxxx;
+		endcase
+	end
+	
+	// Decide what the immediate is according to the intruction type
+	// All immediates are sign extended to 32 bits
+	always @(*) begin
+		case(inst_type)
+			R_TYPE: begin
+				// R_TYPE has no immediate
+				imm = 32'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
+			end
+			I_TYPE: begin
+				imm = {{21{inst[31]}}, inst[30:25], inst[24:21], inst[20]};
+			end	
+			S_TYPE: begin
+				imm = {{21{inst[31]}}, inst[30:25], inst[11:8], inst[7]};
+			end	
+			B_TYPE: begin
+				imm = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0};
+			end	
+			U_TYPE: begin
+				imm = {inst[31], inst[30:20], inst[19:12], 12'b000000000000};
+			end	
+			J_TYPE: begin
+				imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0};
+			end	
+			default: begin
+				imm = 32'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
+			end
+		endcase
+	end
+
+	assign rs2 = inst[24:20];
+	assign rs1 = inst[19:15];
+	assign rd  = inst[11:7];
+
+	assign opcode = inst[6:0];
+
+endmodule
---
+++ tb/tb_Decoder.v
@@ -0,0 +1,81 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/08/2021 12:35:46 PM
+// Design Name: 
+// Module Name: tb_Decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_Decoder(
+
+    );
+
+	// Decoder inputs and outputs
+	reg		[31:0]	inst;
+        
+    wire	[6:0]	funct7;
+    wire	[2:0]	funct3;
+    wire	[31:0]	imm;
+    wire	[4:0]	rs2;
+    wire	[4:0]	rs1;
+    wire	[4:0]	rd;
+    wire	[6:0]	opcode;
+
+	// clock signal
+	reg	sim_clk;
+
+	// clock period
+	parameter CLK_PERIOD = 10;
+	
+	// Module to test
+	Decoder decoder(
+		.inst(inst),
+        .funct7(funct7),
+        .funct3(funct3),
+        .imm(imm),
+        .rs2(rs2),
+        .rs1(rs1),
+        .rd(rd),
+        .opcode(opcode)
+	);          
+
+
+	// generate clock
+	initial sim_clk = 1'b0;
+
+	always #(CLK_PERIOD/2) begin
+		sim_clk = ~sim_clk;
+	end
+	
+	initial begin
+		@(posedge sim_clk);
+		inst = 32'b111111111100_00011_010_00101_0000011; // lw x5, -4(x3)
+
+		@(posedge sim_clk);
+		inst = 32'b1111111_00101_00011_010_11100_0100011; // sw x5, -4(x3)
+
+		@(posedge sim_clk);
+		inst = 32'b1111111_00101_00011_100_11110_1100011; // blt x3, x5, -2050
+
+
+		forever begin
+			@(posedge sim_clk);
+		end
+
+	end
+
+endmodule
